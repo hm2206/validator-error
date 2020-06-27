a1a07df0d918a1ba6ab505c1ed98b5b809b7ce2a
Hans <twd2206@gmail.com>
2020-06-27 13:04:17 -0500

    fixed errors

--- src/Validation.js
+++ src/Validation.js
@@ -2,7 +2,7 @@ const ValidatorError = require('./ValidatorError');
 
 module.exports = (validate, request = {}, rules = {}) => {
     if (typeof validate != 'function') throw new Error("El parametro validate debe de ser una función");
-    return validar.then(res => {
+    return validate(request, rules).then(res => {
         if (res.fails()) throw new ValidatorError(res.messages());
     });
 }
