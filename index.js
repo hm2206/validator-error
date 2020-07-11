6b961ff6adbe286fd335140fc172303cef85f027
Hans <twd2206@gmail.com>
2020-07-11 11:05:11 -0500

    add index storage

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const ValidatorError = require('./src/ValidatorError'); 
 const RequestParse = require('./src/RequestParse');
 const validation = require('./src/Validation');
+const Storage = require('./src/Storage');
 
 
 module.exports = {
     ValidatorError,
     RequestParse,
-    validation
+    validation,
+    Storage
 }
