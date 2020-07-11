55f4bc83a4ce08e479737ba72f6cf90b145d9f85
Hans <twd2206@gmail.com>
2020-07-11 12:29:03 -0500

    fixed storage success

--- src/Storage.js
+++ src/Storage.js
@@ -3,6 +3,12 @@ const ValidatorError = require('./ValidatorError');
 const saveFile = async (request, name, config = { required: false }, Helpers, upload = { path: "upload", options: { name: "", overwrite: false } }) => {
     let file = request.file(name, config);
     if (config.required && !file) throw new ValidatorError([ { field: name, message: `el archivo ${name} es requerido` } ]);
+    if (!file) return {
+        success: false,
+        code: "NOT_FOUND_FILE",
+        message: "No se encontró el archivo"
+    };
+    // next
     upload.options.name = `${upload.options.name}.${file.extname}`;
     await file.move(Helpers.tmpPath(upload.path), upload.options);
     if (!file.moved()) {
@@ -16,6 +22,9 @@ const saveFile = async (request, name, config = { required: false }, Helpers, up
     }
     // response
     return {
+        success: true,
+        code: "SAVE_SUCCESS",
+        message: "El archivo se guardo correctamente",
         realPath: Helpers.tmpPath(`${upload.path}/${upload.options.name}`),
         path: `${upload.path}/${upload.options.name}`,
         name: upload.options.name,
