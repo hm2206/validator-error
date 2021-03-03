2a6b56b6169de5e8304ae922787a3a3f93cdda19
Hans <twd2206@gmail.com>
2021-03-03 10:50:24 -0500

    wrap adoins

--- src/Validation.js
+++ src/Validation.js
@@ -7,15 +7,21 @@ const locationes = [
 
 module.exports = (validate = null, data = {}, rules = {}, messages = {}, location = 'es') => {
     if (typeof validate != 'function') validate = validator.validateAll;
+    let is_adonis = validate ? true : false;
     // obtener idíoma
     if (locationes.includes(location)) {
         let location_messages = require(`./lang/${location}.js`);
         messages = { ...location_messages, ...messages };
     }
+    // validar si es adonis
+    if (is_adonis) return validate(data, rules, message)
+        .then(res => {
+            if (!res.fails()) throw new ValidatorError(res.messages());
+            return res.data;
+        });
     // response
-    return validate(data, rules, messages).then(res => {
-        return res;
-    }).catch(errors => {
+    return validate(data, rules, messages).then(res => res)
+    .catch(errors => {
         throw new ValidatorError(errors);
     });
 }
